042d6921f2bab50df0d673adf836401bc0529b92
Marta Sušek <m.susek@live.com>
2020-04-02 20:45:15 +0200

    Add searchBar component.

---
+++ src/components/search.component.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import styled from 'styled-components'
+import searchIcon from '../assets/searchIcon.png'
+const Container = styled.div`
+    height: 60px;
+    display: flex;
+    align-items: center;  
+    justify-content: center;
+    width: 100%;
+    border: 3px solid #777777;
+    border-radius: 8px;
+    padding: 18px;
+`
+const Input = styled.input`
+    background-color: transparent;
+    border: none;
+    width: 100%;
+    margin-left: 18px;
+    color: ${(props)=> props.theme.colors.font_dark};
+    height: 23px;
+    font-size: 20px;
+    &:focus{
+        border: none;
+        outline: none;
+    }
+    `
+const SearchBar = () => {
+
+const [state, setstate] = useState("")
+return(
+        <Container>
+            <img src={searchIcon} alt="Search Icon"/>
+            <Input type="text" placeholder="Search by book name"onChange={(e)=> setstate(e.target.value)} />
+        </Container>
+)}
+export default SearchBar
