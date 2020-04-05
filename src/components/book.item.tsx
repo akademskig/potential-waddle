40be9dad08676a1ef098ecab9794fe9ebd5d36ef
Marta Sušek <m.susek@live.com>
2020-04-05 12:32:27 +0200

    Finish book.item component.

--- src/components/book.item.tsx
+++ src/components/book.item.tsx
@@ -1,30 +1,54 @@
 import React from 'react';
 import styled from 'styled-components'
 import { Link } from 'react-router-dom';
-
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const Container = styled.div`
+    @import 'loaders.css/src/animations/semi-circle-spin.scss';
     display: flex;
     flex-direction: column;
-    margin-top: 1em;
     width: 100%;
 `
 const BookText = styled.div`
     font-size: 22px;
-    color: ${(props)=> props.theme.colors.font_light};
+    font-weight: bold;
+    color: ${(props) => props.theme.colors.font_light};
+    margin: 1em 0;
+    p{
+        display: inline-block;
+        font-size: 16px;
+        color: #999999;
+        font-weight: 500;
+        span{
+            font-weight: bold;
+            color: ${(props) => props.theme.colors.font_light}
+        }
+    }
 `
 
 const BookItem = ({ book }: { book: any }) => {
 
+    const image = {
+        src: book.image,
+        alt: "Book cover",
+        width: "100%"
+    }
     return (
         <Container>
-            <Link to={`${book/book.id}`}></Link>
-            <img width="100%"src={book.image}alt="" />
-            <BookText>{book.name}
-            <p>Owned by{book.owner}</p>
-            
+            <Link to={`${book / book.id}`}>
+                <BookImage image={image} />
+            </Link>
+            <BookText>{book.name}<br />
+                <p>Owned by <span>{book.owner}</span></p>
             </BookText>
         </Container>
     )
 }
+
+const BookImage = ({ image }: { image: any }) => (
+    <LazyLoadImage
+        alt={image.alt}
+        width={image.width}
+        src={image.src} />
+);
 export default BookItem
