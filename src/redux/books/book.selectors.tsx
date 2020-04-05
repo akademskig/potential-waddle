efee1c99c174bbd23a8bed350d18dc8ae74c42b3
Marta Sušek <m.susek@live.com>
2020-04-05 20:09:28 +0200

    Add loading selector + minor modifications.

--- src/redux/books/book.selectors.tsx
+++ src/redux/books/book.selectors.tsx
@@ -16,11 +16,11 @@ export const selectFirstBookGroup = createSelector(
     (bookGroups) => bookGroups.length ? bookGroups[0] : undefined
 )
 export const selectGroupValues = (group: string | undefined) => createSelector(
-    [selectBook, selectBookGroups],
-    (book, bookGroups) => {
+    [selectBookItems, selectBookGroups],
+    (bookList, bookGroups) => {
         const bookGroupMap = new Map()
         bookGroups.forEach((bG: string) => {
-            const groupValues = uniq(book.bookList.map((book: any) => book[bG])).sort()
+            const groupValues = uniq(bookList.map((book: any) => book[bG])).sort()
             bookGroupMap.set(bG, groupValues)
         })
         if (group)
@@ -30,6 +30,10 @@ export const selectGroupValues = (group: string | undefined) => createSelector(
 )
 
 export const selectBookItemsByGroupValue = (group: string | undefined, groupValue: string) => createSelector(
-    [selectBook],
-    (book) => book.bookList.filter((book: any) => (group && group === "random") || (group && book[group] === groupValue))
+    [selectBookItems],
+    (bookList) => bookList.filter((book: any) => (group && group === "random") || (group && book[group] === groupValue))
+)
+
+export const selectLoading = createSelector(
+    [selectBook], (book) => book.loading
 )
