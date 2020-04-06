21076fb30c47f6b30da98b732a7cf40d4303e389
Marta Sušek <m.susek@live.com>
2020-04-06 20:44:54 +0200

    routes: redirect * route to default.

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,16 +3,21 @@ import React from 'react';
 import {
     Switch,
     Route,
-  } from "react-router-dom";
+    Redirect,
+} from "react-router-dom";
+
 import MainPage from './pages/main.page';
 import BookPage from './pages/book.page';
+import { useSelector } from 'react-redux';
+import { selectFirstBookGroup } from './redux/books/book.selectors';
 
-const MainRoutes = () =>{
+const MainRoutes = () => {
+    const firstGroup = useSelector(selectFirstBookGroup)
     return (
         <Switch>
-            <Route  path="/home/:group" component={MainPage}/>
-            <Route  path="/book/:id" component={BookPage}/>
-            <Route  path="*" component={MainPage}/>
+            <Route path="/home/:group" component={MainPage}/>
+            <Route path="/book/:id" component={BookPage} />
+            <Route path="*" component={() => <Redirect to={`/home/${firstGroup}`}/>} />
         </Switch>
     )
 }
