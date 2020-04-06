17021cba1749f8e9b3f4bc9c5fc853bfd9747581
Marta Sušek <m.susek@live.com>
2020-04-06 12:43:12 +0200

    Move theme into a separate file.

---
+++ src/theme/index.tsx
@@ -0,0 +1,20 @@
+
+const theme = {
+    colors: {
+      header_bg: '#535353',
+      main_bg: '#333333',
+      font_dark: '#5A5A5A',
+      font_light: '#CCCCCC',
+      font_medium: "#AAAAAA",
+      border: '#535353'
+    },
+    breakpoints: {
+      xs: '320px',
+      sm: '600px',
+      md: '960px',
+      lg: '1280px',
+      xl: '1920px'
+    }
+  }
+
+  export default theme
