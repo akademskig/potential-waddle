17689af715c3d0c79c2a9ac72634497bb99645e8
Marta Sušek <m.susek@live.com>
2020-04-06 21:14:25 +0200

    bookDetails.component:  refactor bookDetailsView.

--- src/components/bookDetails.component.tsx
+++ src/components/bookDetails.component.tsx
@@ -5,24 +5,40 @@ import { Link } from 'react-router-dom';
 
 
 const BookContainer = styled.div`
-display: flex;
-border-bottom: solid 2px ${(props) => props.theme.colors.border};
-padding-bottom: 4em;
-@media screen and (max-width: ${(props) => props.theme.breakpoints.md}){
-        flex-direction: column;
-    }
-`
-
+    display: flex;
+    border-bottom: solid 2px ${(props) => props.theme.colors.border};
+    padding-bottom: 4em;
+    @media screen and (max-width: ${(props) => props.theme.breakpoints.md}){
+            flex-direction: column;
+        }
+    `
 const BookImageC = styled.div`
-    min-width: 300px;
+    width: 40%;
     max-width: 340px;
+    min-width: 300px;
     padding-top: 1em;
     @media screen and (max-width: ${(props) => props.theme.breakpoints.xs}){
         width: 100%;
     }
     `
 
-const BookDetails = styled.div`
+const BackLink = () => {
+    return (
+        <Link to="/home">
+            {"< Back to collection"}
+        </Link>
+    )
+}
+
+const BackLinkStyled = styled.span`
+    a{
+        color: ${(props) => props.theme.colors.font_dark};
+        font-weight: bold;
+        font-size: 20px;
+    }
+    `
+
+const BookDetailsStyled = styled.div`
     margin-left: 2%;
     padding-top: 1em;
     color: ${(props) => props.theme.colors.font_medium};
@@ -55,65 +71,53 @@ const BookDetails = styled.div`
         line-height: 1.2em;
     }
     `
-
-const BackLink = () => {
+const BookDetails = ({ book }: { book: any }) => {
     return (
-        <Link to="/home">
-            {"< Back to collection"}
-        </Link>
-    )
+        <BookDetailsStyled>
+            <div className="book-title">
+                <h4>{book.name} (<span>{book.year}) </span></h4>
+            </div>
+            <div className="book-details">
+                <ul>
+                    <li>
+                        <span> Writer: </span>  {book.writer}
+                    </li>
+                    <li>
+                        <span> Artist: </span>{book.artist}
+                    </li>
+                    <li>
+                        <span> Publication: </span> {book.publication}
+                    </li>
+                    <li>
+                        <span> Owner: </span> {book.owner}
+                    </li>
+                </ul>
+            </div>
+            <div className="book-summary">
+                {book.summary}
+            </div>
+        </BookDetailsStyled>
 
+    )
 }
 
-const BackLinkStyled = styled.span`
-    a{
-        color: ${(props) => props.theme.colors.font_dark};
-        font-weight: bold;
-        font-size: 20px;
-    }
-    `
 
 const BookDetailsView = ({ book }: { book: any }) => {
     const image = {
         src: book.image,
         width: '100%',
-        alt: "Book cover"   
+        alt: "Book cover"
     }
     return (
         <Fragment>
-            <BackLinkStyled >
-                <BackLink></BackLink>
+            <BackLinkStyled>
+                <BackLink />
             </BackLinkStyled>
             <BookContainer>
                 <BookImageC>
                     <BookImage image={image} />
                 </BookImageC>
-                <BookDetails>
-                    <div className="book-title">
-                        <h4>{book.name} (<span>{book.year}) </span></h4>
-
-                    </div>
-                    <div className="book-details">
-                        <ul>
-                            <li>
-                                <span> Writer: </span>  {book.writer}
-                            </li>
-                            <li>
-                                <span> Artist: </span>{book.artist}
-                            </li>
-                            <li>
-                                <span> Publication: </span> {book.publication}
-                            </li>
-                            <li>
-                                <span> Owner: </span> {book.owner}
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="book-summary">
-                        {book.summary}
-                    </div>
-
-                </BookDetails>
+                <BookDetails book={book} />
             </BookContainer>
         </Fragment>
     )
